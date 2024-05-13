bb2d9c3f2e2d187f4709e101e3d98e55a130a321
SbabalweRoux <sbaviaroux@gmail.com>
2024-05-13 11:41:19 +0200

    Fixed the naming of the variables

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
 /**
- * Debugging Guide
+ * debugging Guidelines
  * 1. Make the code more readable
  * 2. Pick up calculation errors
  * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-let acc = 3; // acceleration (m/s^2)
+const initialVelocity = 10000; // initialVelocity (km/h)
+let acceleration = 3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
+const initialDistance = 0; // initialDistance (km)
 const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
-acc *= 3.6;
+acceleration *= 3.6;
 
 // Calculate remaining fuel
-const calcRemainingFuel = (fuel, fbr, time) => {
-  let uf = 0;
+const calcRemainingFuel = (fuel, fuelBurnRate, time) => {
+  let usedFuel = 0;
   if (
     typeof fuel !== "number" ||
-    typeof fbr !== "number" ||
+    typeof fuelBurnRate !== "number" ||
     typeof time !== "number"
   ) {
     throw new Error(
       "Invalid input. Please provide numeric values for fuel, fuel burn rate, and time."
     );
   } else {
-    uf = fbr * time;
-    return fuel - uf;
+    usedFuel = fuelBurnRate * time;
+    return fuel - usedFuel;
   }
 };
 
 // Pick up an error with how the function below is called and make it robust to such errors
-const calcNewVel = (acc, vel, time) => {
+const calcNewVelocity = (acceleration, initialVelocity, time) => {
   let v = 0;
   if (
-    typeof vel !== "number" ||
-    typeof acc !== "number" ||
+    typeof initialVelocity !== "number" ||
+    typeof acceleration !== "number" ||
     typeof time !== "number"
   ) {
     throw new Error(
       "Invalid input. Please provide numeric values for velocity, acceleration, and time."
     );
   }
-  if (acc === 3) {
+  if (acceleration === 3) {
     throw new Error("You still have to convert acceleration!!");
   } else {
-    v = acc * time;
-    return vel + v;
+    v = acceleration * time;
+    return initialVelocity + v;
   }
 };
 
-const calcNewDistance = (vel, time) => {
-  return vel * (time / 3600);
+const calcNewDistance = (initialVelocity, time) => {
+  return initialVelocity * (time / 3600);
 };
 
-const d2 = d + calcNewDistance(vel, time); //calculates new distance
-const rf = calcRemainingFuel(fuel, fbr, time); //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time); //calculates new velocity based on acceleration
+const newDistance = initialDistance + calcNewDistance(initialVelocity, time); //calculates new distance
+const remainingFuel = calcRemainingFuel(fuel, fuelBurnRate, time); //calculates remaining fuel
+const newVelocity = calcNewVelocity(acceleration, initialVelocity, time); //calculates new Velocity based on acceleration
 
-console.log(`Corrected New Velocity: ${vel2} km/h`);
-console.log(`Corrected New Distance: ${d2} km`);
-console.log(`Corrected Remaining Fuel: ${rf} kg`);
+console.log(`Corrected New velocity: ${newVelocity} km/h`);
+console.log(`Corrected New Distance: ${newDistance} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
